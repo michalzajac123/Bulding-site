d02fcae8eb0cd751af5a909ca11adf0629d6ae0e
Michal Zajac <emmadajo58@gmail.com>
2025-05-01 09:22:52 +0200

    Initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,11 @@
+import { createMemoryHistory, createRouter } from "vue-router";
+
+const routes = [
+  { path: "/", component: () => import("../components/HelloWorld.vue") },
+];
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
+export default router;
