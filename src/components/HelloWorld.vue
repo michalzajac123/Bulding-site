c15b8cfecf2de0861a796d289a94e50bbf32f18f
Michal Zajac <emmadajo58@gmail.com>
2025-05-01 09:36:32 +0200

    Add tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
@@ -17,9 +17,12 @@ const count = ref(0)
     </p>
   </div>
 
-  <p>
+  <p class="">
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+    <a
+      class="text-red-500"
+      href="https://vuejs.org/guide/quick-start.html#local"
+      target="_blank"
       >create-vue</a
     >, the official Vue + Vite starter
   </p>
